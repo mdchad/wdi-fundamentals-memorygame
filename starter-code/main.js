7d57e2a94c7e5708c66390e189130b6b69410ca3
chad <wildfootballer@hotmail.com>
2016-08-12 18:50:42 +0800

    memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,30 @@
+console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//var cardOne = "queen";
+//var cardTwo = "queen";
+//var cardThree = "king";
+//var cardFour = "king";
+
+//if (cardOne === cardTwo) {
+ 	//alert('You found a match');
+//}
+
+//else if (cardOne === cardThree) {
+	//alert('Sorry, try again');
+//}
+
+//else if (cardOne === cardFour) {
+	//alert("Sorry, try again");
+//}
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+	for (var i = i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+	cardElement.className = 'card';
+
+	board.appendChild(cardElement);
+
+	}
+}
