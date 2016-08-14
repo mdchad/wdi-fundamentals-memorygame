7b404b4b02a36d28aae0085374a1c4b6568f0b4b
chad <wildfootballer@hotmail.com>
2016-08-15 07:52:42 +0800

    memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,21 +1,7 @@
 console.log("JS file is connected to HTML! Woo!")
 
-//var cardOne = "queen";
-//var cardTwo = "queen";
-//var cardThree = "king";
-//var cardFour = "king";
-
-//if (cardOne === cardTwo) {
- 	//alert('You found a match');
-//}
-
-//else if (cardOne === cardThree) {
-	//alert('Sorry, try again');
-//}
-
-//else if (cardOne === cardFour) {
-	//alert("Sorry, try again");
-//}
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 var board = document.getElementById('game-board');
 
@@ -24,7 +10,34 @@ function createBoard() {
 		var cardElement = document.createElement('div');
 	cardElement.className = 'card';
 
-	board.appendChild(cardElement);
+	board.appendChild(cardElement)
 
 	}
+}
+
+//checks to see if there are cards in play
+function isTwoCards() {
+
+  // add card to array of cards in play
+  // 'this' hasn't been covered in this prework, but
+  // for now, just know it gives you access to the card the user clicked on
+  cardsInPlay.push(this.getAttribute('data-card'));
+  console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
+	}
+
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+
+  }
+
 }
